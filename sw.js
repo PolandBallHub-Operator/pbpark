33db0a4273bee30f21ea0621b38270ec947984a8
DeveloperTangle <polandballhub@gmail.com>
2026-06-13 12:26:25 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,41 @@
-var CACHE_NAME = 'pbhub';
+---
+---
+var CACHE_NAME = 'pbhub-v1';
 var urlsToCache = [
-	'/pbhub/',
+  '{{ site.baseurl }}/',
+  '{{ site.baseurl }}/index.html',
+  '{{ site.baseurl }}/manifest.json',
+  '{{ site.baseurl }}/images/icon-192.png',
+  '{{ site.baseurl }}/images/icon-512.png'
 ];
 
 self.addEventListener('install', function(event) {
-	event.waitUntil(
-		caches
-			.open(CACHE_NAME)
-			.then(function(cache) {
-				return cache.addAll(urlsToCache);
-			})
-	);
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
 self.addEventListener('fetch', function(event) {
-	event.respondWith(
-		caches
-			.match(event.request)
-			.then(function(response) {
-				return response ? response : fetch(event.request);
-			})
-	);
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response ? response : fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  var cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
 });
